25b5e45804b0c620f14204431413382766d4b799
Kashif <shafatkashif@gmail.com>
2023-05-04 06:11:20 -0700

    add Portals and Ref

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
@@ -13,10 +13,10 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <Fragment className="App">
       <AddUser AddUser={addUserHandler} />
       <UserList users={userList} />
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/UI/ErrorMode.js
+++ src/components/UI/ErrorMode.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React,{Fragment} from "react";
 import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
 
 const ErrorMode = (props) => {
-  return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+  };
+  const OverLay = (props) => {
+    return (
       <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>OKAY</Button>
-        </footer>
-      </Card>
-    </div>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>OKAY</Button>
+      </footer>
+    </Card>
+    );
+  };
+  return (
+    <Fragment>
+    {ReactDOM.createPortal(
+      <Backdrop onConfirm={props.onConfirm} />,
+      document.getElementById("backdrop_root")
+    )}
+    {ReactDOM.createPortal(
+      <OverLay
+        title={props.title}
+        message={props.message}
+        onConfirm={props.onConfirm}
+      />,
+      document.getElementById("modal_root")
+    )}
+  </Fragment>
   );
 };
 
